898b2e92d12c5dba577680cfe791cd607db148ec
Glen Pierce <glen.piercev@gmail.com>
2020-02-01 13:23:43 -0800

    humans move... broken

--- Assets/Scripts/HumanController.cs
+++ Assets/Scripts/HumanController.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Component;
 
 
 public class HumanController : MonoBehaviour {
 
+    [SerializeField] private Vector2[] patrolPoints;
+    [SerializeField] private float alertRadius = 10;
+    [SerializeField] private Movement movement;
+    bool canMove = true;
+
     void Start() {
-        
+        movement = GetComponent<Movement>();
     }
 
     void Update() {
-        
+        if(!movement.isMoving() && canMove) {
+            canMove = false;
+            movement.Move(Data.MovementDirection.NORTH);
+        }
     }
 }
