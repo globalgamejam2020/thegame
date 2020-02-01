7741d2413d4ce8d87bca172e5ebd1f7ed1ebec8e
Glen Pierce <glen.piercev@gmail.com>
2020-02-01 15:04:58 -0800

    movement

--- Assets/Scripts/Components/Movement.cs
+++ Assets/Scripts/Components/Movement.cs
@@ -5,45 +5,47 @@ using UnityEngine;
 
 namespace Component {
     public class Movement : MonoBehaviour {
-        [SerializeField] private float Speed = 0.05f;
-        [SerializeField] private Vector2 Position = Vector2.zero;
+        [SerializeField] private float Speed;
 
-        private Vector2 Change = Vector2.zero;
+        private Vector3 origin = Vector3.zero;
+        private Vector3 destination = Vector3.zero;
 
         void Start() {
+            origin = this.transform.position;
+            destination = this.transform.position;
         }
 
         void Update() {
             if (isMoving()) {
-                Position = Position + (Change * Time.deltaTime);
-                this.transform.Translate(Position);
-                Change -= Change * Time.deltaTime;
-                Debug.Log(Change.magnitude);
-                if(Change.magnitude < (Change.magnitude * Time.deltaTime)) {
-                    Change = Vector2.zero;
+                float distance = Speed * Time.deltaTime;
+                this.transform.Translate(distance * (destination - origin));
+                Debug.Log(destination - origin);
+                var closeEnough = (this.destination - this.transform.position).sqrMagnitude < 0.1f;
+                if(closeEnough) {
+                    this.transform.position = destination;
+                    origin = destination;
                 }
             }
         }
 
         public bool isMoving() {
-            return Change != Vector2.zero;
-        }
-
-        public Vector2 getChange() {
-            return Change;
+            return destination != origin;
         }
 
         public void Move(MovementDirection direction) {
+            if(isMoving())
+                return;
+            Debug.Log("moving");
             if (direction.Matches(MovementDirection.NORTH)) {
-                Change.y = 1f * Speed;
+                destination.y += 1f;
             } else if (direction.Matches(MovementDirection.SOUTH)) {
-                Change.y = -1f * Speed;
+                destination.y += -1f;
             }
 
             if (direction.Matches(MovementDirection.EAST)) {
-                Change.x = 1f * Speed;
+                destination.x += 1f;
             } else if (direction.Matches(MovementDirection.WEST)) {
-                Change.x = -1f * Speed;
+                destination.x += -1f;
             }
         }
     }
