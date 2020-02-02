d0217c6016f8a86b6accd43703cba2bbd5a484db
Glen Pierce <glen.piercev@gmail.com>
2020-02-02 12:30:03 -0800

    tree growth

--- Assets/Scripts/Components/Poop.cs
+++ Assets/Scripts/Components/Poop.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Component
-{
-    public class Poop : MonoBehaviour
-    {
+namespace Component {
+    public class Poop : MonoBehaviour {
         [SerializeField] private GameObject poop;
         private Vector3 position;
         [SerializeField] private int effectRadius;
@@ -14,9 +12,9 @@ namespace Component
         private SpriteRenderer spriteRenderer;
 
         private int animationFrame = 0;
+        private bool shouldUpdate = true;
 
-        public Poop(Vector3 position)
-        {
+        public Poop(Vector3 position) {
             LoadSprites();
 
             this.position = position;
@@ -30,35 +28,37 @@ namespace Component
             SetPoopComponent(poopComponent);
         }
 
-        private void SetPoopComponent(Poop poo)
-        {
+        private void SetPoopComponent(Poop poo) {
             poo.effectRadius = this.effectRadius;
             poo.spriteRenderer = poo.GetComponent<SpriteRenderer>();
             poo.poopSprites = this.poopSprites;
             poo.position = this.position;
+            poo.shouldUpdate = true;
+            Destroy(poo.GetComponent<GameObject>(), 2f);
             poo.InvokeRepeating("AnimatePoop", 0f, 0.15f);
         }
 
-        private void LoadSprites()
-        {
-            for (int i = 0; i < 3; i++)
-            {
+        private void LoadSprites() {
+            for (int i = 0; i < 3; i++) {
                 poopSprites[i] = Resources.Load<Sprite>("Sprites/poop" + i);
             }
         }
 
-        private void AnimatePoop()
-        {
+        private void AnimatePoop() {
             if (animationFrame > effectRadius) { CancelInvoke(); return; }
 
             spriteRenderer.sprite = poopSprites[animationFrame];
             animationFrame++;
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-            //tree radius growing code
+        void Update() {
+            if(shouldUpdate) {
+                Destroy(this, 2.0f);
+                GameObject tree = Resources.Load<GameObject>("GameObjects/Tree");
+                Debug.Log(tree);
+                Instantiate(tree, this.transform.position, Quaternion.identity);
+                shouldUpdate = false;
+            }
         }
     }
 }
