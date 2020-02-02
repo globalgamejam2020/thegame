f2fe0e03438a35d512d672329c467ad2458207ca
Sarp Saysel <sarp@jostle.me>
2020-02-02 15:39:46 -0800

    Tree grass growing and sorting layer

--- Assets/Scripts/TreeController.cs
+++ Assets/Scripts/TreeController.cs
@@ -5,6 +5,9 @@ using Component;
 
 public class TreeController : MonoBehaviour
 {
+    private GameObject grass1; 
+    private GameObject grass2;
+
 
     [SerializeField] int treeStrength = 1;
     private Animator animator;
@@ -12,6 +15,9 @@ public class TreeController : MonoBehaviour
     private void Start()
     {
         animator = GetComponent<Animator>();
+        grass1 = Resources.Load<GameObject>("GameObjects/Grass1"); 
+        grass2 = Resources.Load<GameObject>("GameObjects/Grass2");
+
     }
 
     public void PlantTree(int strength)
@@ -29,11 +35,23 @@ public class TreeController : MonoBehaviour
     public void Sprout()
     {
         animator.SetTrigger("Sprout");
-        Invoke("Grow", 5f / treeStrength);
+        Invoke("Grow", 10f / treeStrength);
     }
 
     public void Grow()
     {
         animator.SetTrigger("Grow");
+        Invoke("GrowGrass1", 10f);
+    }
+
+    public void GrowGrass1()
+    {
+        Instantiate(grass1, transform.position, Quaternion.identity);
+        Invoke("GrowGrass2", 10f);
+    }
+
+    public void GrowGrass2()
+    {
+        Instantiate(grass2, transform.position, Quaternion.identity);
     }
 }
