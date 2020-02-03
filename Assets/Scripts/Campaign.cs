2b4ec0a7f25499cdfa3902e18fb91f116d243ed7
Jennifer Pierce <jen@jennifers-mbp.wifi.bcit.ca>
2020-02-02 16:21:10 -0800

    scene work

---
+++ Assets/Scripts/Campaign.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Campaign : MonoBehaviour
+{
+  private AudioSource audioSource;
+  public List<AudioClip> clips;
+  public List<GameObject> scenes;
+  public GameObject currentScene;
+  private int currIndex = 0;
+  public float timeUntilNextAudio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = clips[0];
+        audioSource.Play();
+        scenes[0].SetActive(true);
+        for (int i = 1; i < scenes.Count; i++){
+          scenes[i].SetActive(false);
+        }
+    }
+
+    void Update(){
+      if (Input.GetKeyDown(KeyCode.Space)){
+        Next();
+      }
+    }
+
+    public void Next(){
+      scenes[currIndex].SetActive(false);
+      scenes[currIndex].SetActive(true);
+      currIndex++;
+      audioSource.clip = clips[currIndex];
+      audioSource.Play();
+    }
+}
