014cbda773b308a717ee2b2101e6b725b1d777db
Glen Pierce <glen.piercev@gmail.com>
2020-02-08 10:13:51 -0800

    scene updates for scale

--- Assets/Scripts/Components/Squirrel.cs
+++ Assets/Scripts/Components/Squirrel.cs
@@ -9,34 +9,27 @@ using Vector3 = UnityEngine.Vector3;
 namespace Component {
     public class Squirrel : MonoBehaviour {
         private bool rabid = false;
-        private float overlapRadius;
-
         private Movement movement;
         private AnimationController animationController;
         private Animator animator;
         private AudioController audioController;
+        // private Tree
 
         void Start() {
             movement = GetComponent<Movement>();
             animationController = GetComponent<AnimationController>();
             audioController = GetComponent<AudioController>();
             animator = GetComponent<Animator>();
-
-            float rabidInvoke = Random.Range(10f, 20f);
-            Invoke("MakeRabid", rabidInvoke);
-            Invoke("MoveRandom", 2f);
         }
 
-        // Update is called once per frame
         void Update() {
-            if (rabid && !IsInvoking("Attack")) Invoke("Attack", 0f);
+            
         }
 
         private void MoveRandom() {
-            if (!movement.Move(GetDirection())) movement.Move(RandomMoveDirection());
-
-            float nextInvoke = Random.Range(1f, 5f);
-            Invoke("MoveRandom", nextInvoke);
+            if (!movement.Move(GetDirection())) {
+                movement.Move(RandomMoveDirection());
+            }
 
             if (!rabid)
                 audioController.PlayFX("move");
@@ -47,7 +40,8 @@ namespace Component {
         private MovementDirection GetDirection() {
             var treePosition = FindTreeToMoveTo();
 
-            if (ReferenceEquals(treePosition, null)) return RandomMoveDirection();
+            if (ReferenceEquals(treePosition, null))
+                return RandomMoveDirection();
             return DirectionTo((Vector3) treePosition);
         }
 
@@ -72,70 +66,34 @@ namespace Component {
                 direction |= MovementDirection.EAST;
             else if (position.x > treePosition.x) direction |= MovementDirection.WEST;
 
-            // Debug.Log($"Found tree! Will move to it: {direction}, {treePosition}, {position}");
-
             return direction;
         }
 
         private Vector3? FindTreeToMoveTo() {
-            var (closest, distance) = ((GameObject) null, 0f);
+            GameObject[] trees = GameObject.FindGameObjectsWithTag("tree");
 
-            foreach (var direction in new Vector3[] {
-                transform.up,
-                transform.right,
-                transform.right * -1f,
-            }) {
-                var (obj, d) = FindClosestTree(Physics2D.CircleCastAll(this.transform.position, 5, direction));
-                if (obj == null) continue;
-                if (closest != null && distance < d) continue;
-                closest = obj;
-                distance = d;
-            }
-
-            if (closest == null || distance < 1f) {
-                // animator.SetBool("InTree", true);
-                return null;
-            } else {
-                // animator.SetBool("InTree", false);
-                return closest.transform.position;
-            }
-        }
-
-        private (GameObject, float) FindClosestTree(RaycastHit2D[] hits) {
-            var distance = 0f;
-            GameObject closest = null;
-            foreach (var hit in hits) {
-                var obj = hit.transform.gameObject;
-                if (!obj.name.Contains("Tree")) continue;
-
-                var thisPosition = this.transform.position;
-                var objDistance = (thisPosition - obj.transform.position).sqrMagnitude;
-
-                if (closest == null) {
-                    closest = obj;
-                    distance = objDistance;
-                } else {
-                    var closestDistance = (thisPosition - closest.transform.position).sqrMagnitude;
-                    if (closestDistance < objDistance && closestDistance > 0f) {
-                        closest = obj;
-                        distance = closestDistance;
-                    }
+            foreach (GameObject tree in trees) {
+                RaycastHit2D endPoint = Physics2D.Raycast(this.transform.position, tree.transform.position);
+                if(endPoint.collider != null) {
+                    
                 }
             }
+            return null;
 
-            return (closest, distance);
+            // if (closest == null || distance < 1f) {
+            //     animator.SetBool("InTree", true);
+            //     return null;
+            // } else {
+            //     animator.SetBool("InTree", false);
+            //     return closest.transform.position;
+            // }
         }
 
         private void Attack() {
             animationController.Attack();
-
-            float nextInvoke = Random.Range(0.5f, 3f);
-            Invoke("Attack", nextInvoke);
         }
 
         private void MakeRabid() {
-            Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, overlapRadius);
-
             rabid = true;
             animationController.Rabid();
         }
--- Assets/Scripts/TreeController.cs
+++ Assets/Scripts/TreeController.cs
@@ -3,55 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using Component;
 
-public class TreeController : MonoBehaviour
-{
+public class TreeController : MonoBehaviour {
     private GameObject grass1; 
     private GameObject grass2;
-
-
     [SerializeField] int treeStrength = 1;
     private Animator animator;
 
-    private void Start()
-    {
+    private void Start() {
         animator = GetComponent<Animator>();
         grass1 = Resources.Load<GameObject>("GameObjects/Grass1"); 
         grass2 = Resources.Load<GameObject>("GameObjects/Grass2");
-
     }
 
-    public void PlantTree(int strength)
-    {
+    public void PlantTree(int strength) {
         this.treeStrength = strength;
         Invoke("SparkleSoundFX", 0.2f);
         Invoke("Sprout", 5f / treeStrength);
     }
 
-    private void SparkleSoundFX()
-    {
+    private void SparkleSoundFX() {
         GetComponent<AudioController>().PlayFX("move");
     }
 
-    public void Sprout()
-    {
+    public void Sprout() {
         animator.SetTrigger("Sprout");
         Invoke("Grow", 10f / treeStrength);
     }
 
-    public void Grow()
-    {
+    public void Grow() {
         animator.SetTrigger("Grow");
         Invoke("GrowGrass1", 10f);
     }
 
-    public void GrowGrass1()
-    {
+    public void GrowGrass1() {
         Instantiate(grass1, transform.position, Quaternion.identity);
         Invoke("GrowGrass2", 10f);
     }
 
-    public void GrowGrass2()
-    {
+    public void GrowGrass2() {
         Instantiate(grass2, transform.position, Quaternion.identity);
     }
 }
